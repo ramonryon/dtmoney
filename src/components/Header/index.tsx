0cdded1b004a8e3bae0e5a975194018920e140c6
Ramon Ryon <ramonryon@gmail.com>
2021-11-19 10:25:39 -0300

    Create Compornent TransactionModal and Styled the form and modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
+
 import logoImg from '../../assets/logo.svg'; 
 import { Container, Content } from './styles';
 
-export function Header(){
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+interface HeaderProps{
+    onOpenNewTrasactionModal: () => void;
+}
 
-    function handleOpenNewTransactionModal(){
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal(){
-        setIsNewTransactionModalOpen(false);
-    }
+export function Header({onOpenNewTrasactionModal}: HeaderProps){
+    
 
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button"onClick={handleOpenNewTransactionModal}>
+                <button type="button"onClick={onOpenNewTrasactionModal}>
                     Nova transação
-                </button>
-
-                <Modal
-                    isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                >
-                    <h2>Cadastrar Transação</h2>
-                </Modal>
+                </button>      
             </Content>
         </Container>
     )
