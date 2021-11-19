42554acdb8db02924e73963cdeacb9b5f5d4ad75
Ramon Ryon <ramonryon@gmail.com>
2021-11-19 11:30:03 -0300

    Create Button Type and Styles him

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { useState } from "react";
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -10,6 +11,9 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
+
+    const [type, setType] = useState('deposit');
+
     return(
         <Modal
           isOpen={isOpen}
@@ -31,18 +35,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input placeholder="Título"/>
                 <input type="number" placeholder="Valor" />
                 <TransactionTypeContainer>
-                    <button
+                    <RadioBox
                         type="button"
+                        onClick={() => {setType('deposit');}}
+                        isActive={type === 'deposit'}
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
-                    <button
+                    </RadioBox>
+                    <RadioBox
                         type="button"
+                        onClick={() => {setType('withdraw');}}
+                        isActive={type === 'withdraw'}
                     >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
                 <input placeholder="Categoria" />
                 <button type="submit">Cadastrar</button>
